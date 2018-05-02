06aab456e8f99b8557f082a421579ed51dd3d8d7
27Ronen <RonenSkarsten@gmail.com>
2018-05-02 13:14:00 -0700

    wow

--- src/PeopleAndSchool/Student.java
+++ src/PeopleAndSchool/Student.java
@@ -1,8 +1,47 @@
 package PeopleAndSchool;
 import ScheduleInterfaces.*;
+import java.util.Date;
 
 public class Student extends Person implements GenerateRandomSchedule{
+
     //Data Fields
+    private Date dateOfBirth;
+    private Date startDate;
+    private double gpa;
 
+    public Student()
+    {
 
+    }
+
+    public Student(String firstName, String middleName, String lastName, String schoolName, String uniqueID, String telephone, PersonAddress address, Date dateOfBirth, Date startDate, double gpa) {
+        super(firstName, middleName, lastName, schoolName, uniqueID, telephone, address);
+        this.dateOfBirth = dateOfBirth;
+        this.startDate = startDate;
+        this.gpa = gpa;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+
+    public void setGpa(double gpa) {
+        this.gpa = gpa;
+    }
 }
--- src/PeopleAndSchool/Teacher.java
+++ src/PeopleAndSchool/Teacher.java
@@ -2,8 +2,37 @@ package PeopleAndSchool;
 import java.util.Date;
 
 public class Teacher extends Person {
+
     //Data Fields
     private boolean isTenured;
     private Date hireDate;
+
+    public Teacher()
+    {
+
+    }
+
+    public Teacher(String firstName, String middleName, String lastName, String schoolName, String uniqueID, String telephone, PersonAddress address, boolean isTenured, Date hireDate) {
+        super(firstName, middleName, lastName, schoolName, uniqueID, telephone, address);
+        this.isTenured = isTenured;
+        this.hireDate = hireDate;
+    }
+
+    public boolean isTenured() {
+        return isTenured;
+    }
+
+    public void setTenured(boolean tenured) {
+        isTenured = tenured;
+    }
+
+    public Date getHireDate() {
+        return hireDate;
+    }
+
+    public void setHireDate(Date hireDate) {
+        this.hireDate = hireDate;
+    }
+
 
 }

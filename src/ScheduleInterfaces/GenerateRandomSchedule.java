bc416c24657b691633275bd9415d9ae34cfa3128
Erika Anne Marin <emarin5@ivc.edu>
2018-05-07 13:48:00 -0700

    added Sequential Scheduler interface

--- src/ScheduleInterfaces/GenerateRandomSchedule.java
+++ src/ScheduleInterfaces/GenerateRandomSchedule.java
@@ -1,6 +1,9 @@
 package ScheduleInterfaces;
 
+import java.util.ArrayList;
+
 public interface GenerateRandomSchedule {
 
     //have a method that returns an arrayList of Classes Class
+
 }
---
+++ src/ScheduleInterfaces/GenerateSequentialScheduler.java
@@ -0,0 +1,8 @@
+package ScheduleInterfaces;
+
+/**
+ * Created by emarin5 on 5/7/2018.
+ */
+public interface GenerateSequentialScheduler {
+    //method that returns array list of classes Class
+}
